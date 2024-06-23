e9dfbfd9e78e27aa7a957a81aed02dd6c43695a3
Mark kimaaathi <markangelkimathi@gmail.com>
2024-06-23 13:46:03 +0300

    main screen

---
+++ lib/app_view.dart
@@ -0,0 +1,24 @@
+import 'package:expense_repository/expense_repository.dart';
+import 'package:expenses_tracker/screens/home/blocs/get_expenses_bloc/get_expenses_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'screens/home/views/home_screen.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Outlay Tracker",
+      theme: ThemeData(colorScheme: ColorScheme.light(background: Colors.grey.shade100, onBackground: Colors.black, primary: const Color(0xFF00B2E7), secondary: const Color(0xFFE064F7), tertiary: const Color(0xFFFF8D6C), outline: Colors.grey)),
+      home: BlocProvider(
+        create: (context) => GetExpensesBloc(
+          FirebaseExpenseRepo()
+        )..add(GetExpenses()),
+        child: const HomeScreen(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:bloc/bloc.dart';
 import 'package:expenses_tracker/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'simple_bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
